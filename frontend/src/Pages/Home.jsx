e483ddf6928c1c86284c784cb7100ae999d74737
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-04-24 00:52:24 +0530

    landing page

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import '../assets/home.css';
+
+export function Home(){
+    return (
+        <div className="home">
+            <div className="navbar">
+                <h3>Notely</h3>
+                <a>About</a>
+                <a>Features</a>
+                <a>Contact</a>
+                <a>Check My Portfolio</a>
+                <button className='login'>Login</button>
+                <button>Try for free</button>
+            </div>
+            <h1>All your thoughts at <span>One Place</span> <br></br> without any cost.</h1>
+            <p>A simple and secure note-taking website built to help you organize all your thoughts and ideas. <br />Accessible anytime, anywhere - Your personal space to write.</p>
+            <div className="buttons">
+                <button className='cta1'>Start Jotting</button>
+                <button className='cta2'>Know More</button>
+            </div>
+        </div>
+    )
+}
