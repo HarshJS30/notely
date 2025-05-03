35a107c685e886e95f8f1205fe7d3a8a69de56f1
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-05-03 19:32:12 +0530

    ux of auth pages improved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Features} from "./Pages/Features";
 import { Home } from "./Pages/Home";
 import { Reviews } from "./Pages/Review";
 import { Login } from "./Pages/Login";
+import { Signup } from "./Pages/SIgnup";
 
 function App(){
   return(
@@ -11,6 +12,7 @@ function App(){
       <Route path='/' element={<Features />} />
       <Route path='/' element={<Reviews />} />
       <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<Signup />} />
     </Routes>
 
   )
--- frontend/src/Pages/SIgnup.jsx
+++ frontend/src/Pages/SIgnup.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import '../assets/second.css';
 import { useNavigate } from "react-router-dom";
 
-export function Login() {
-  const navigate = useNavigate();
+export function Signup() {
+    const navigate = useNavigate();
   return (
     <div className="form">
-      <h1>Welcome Back to Notely</h1>
-      <h3>Please enter your details to login</h3>
+      <h1>Welcome to Notely</h1>
+      <h3>Please enter your details to signup</h3>
       <form>
         <label>Email</label>
         <input type="email" placeholder="Enter your email" />
         <label>Password</label>
         <input type="password" placeholder="Enter your password" />
-        <button className="lgn">Login</button>
-        <a onClick={()=>navigate('/signup')}>Dont have an account? Click here</a>
+        <button className="lgn">Sign in</button>
+        <a onClick={()=>navigate('/login')}>Dont have an account? Click here</a>
         <hr className="hr1"/>
         <p>OR</p>
         <hr className="hr2"/>
-        <button className="google">Login with google</button>
+        <button className="google">Sign in with google</button>
       </form>
     </div>
   );
